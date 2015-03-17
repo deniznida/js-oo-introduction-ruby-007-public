08dbfc47e3673f82fe034e956803276ae3466c20
deniznida <denizunat@hotmail.com>
2015-03-17 17:49:04 -0400

    done

--- js/bike.js
+++ js/bike.js
@@ -1,7 +1,23 @@
-function Bike(){
+function Bike(name, type, color){
   // attributes go here...
+  this.name = name;
+  this.bikeType = type;
+  this.frameColor = color;
+  this.condition = "ready to go!";
 }
 
-Bike.prototype.someInstanceMethod = function(){
+Bike.prototype.takeForARide = function(distance){
   // code goes here...
+  if (distance > 50) {
+			this.condition = "needs a tune up";
+		}
+
+};
+
+Bike.prototype.WHEELS = 2;
+
+Bike.prototype.tuneUp = function(){
+  // code goes here...
+  this.condition = "ready to go!";
+
 };
--- js/owner.js
+++ js/owner.js
@@ -1,7 +1,20 @@
-function Owner(){
+function Owner(name){
   // attributes go here...
+  this.name = name;
+  this.bikes = [];
 }
 
-Owner.prototype.someInstanceMethod = function(){
+Owner.prototype.buildBike = function(name, type, color){
   // code goes here...
+  var newBike = new Bike(name, type, color);
+  this.bikes.push(newBike);
+};
+
+Owner.prototype.rideBike = function(bike_name, distance){
+  // code goes here...
+	this.bikes.forEach(function(bike){
+	  if (bike.name === bike_name){
+	  	bike.takeForARide(distance);
+	  }
+	});
 };
